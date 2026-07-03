9b6434dd5207ee66952c21427b216f5b37f44b9d
Logan Wakeling <lwakelin0023@pointloma.edu>
2026-07-03 16:38:10 +0000

    Changes to main file and added facial_regions

--- FacialLandmarkPrototype.py
+++ FacialLandmarkPrototype.py
@@ -2,6 +2,10 @@ import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import facial_regions
+import numpy as np
+
+print("Facial Regions working")
 
 # =========================
 # 1. Create the landmarker
@@ -77,85 +81,45 @@ def process_image(image_path):
     cv2.destroyAllWindows()
 
 
+
+def extract_landmarks(image_path):
+
+    mp_image = mp.Image.create_from_file(image_path)
+    result = detector.detect(mp_image)
+
+    if not result.face_landmarks:
+        return None
+
+    # Get first detected face
+    face_landmarks = result.face_landmarks[0]
+
+    # Store normalized coordinates
+    landmarks = np.array([
+        [lm.x, lm.y, lm.z]
+        for lm in face_landmarks
+    ])
+
+    return landmarks
+
+
 # =========================
-# Process two images
+# Paths of two images
 # =========================
 image_paths = [
     "/home/testuser/Desktop/MediaPipeDeepFakeDetectionPrototype/FacialLandmarkData/test2.jpg",
     "/home/testuser/Desktop/MediaPipeDeepFakeDetectionPrototype/FacialLandmarkData/test3.jpg"
 ]
 
+# ==========================
+#Extracting Landmarks
+# ==========================
+image1 = extract_landmarks(image_paths[0])
+image2 = extract_landmarks(image_paths[1])
+
+
+# ========================================
+#Print out image with landmarks on it
+# ========================================
 for path in image_paths:
     process_image(path)
 
-'''
-# =========================
-# 2. Load image
-# =========================
-image_path = "/home/testuser/Desktop/MediaPipeDeepFakeDetectionPrototype/FacialLandmarkData/test1.jpg"
-
-
-mp_image = mp.Image.create_from_file(image_path)
-opencv_image = cv2.imread(image_path)
-
-# =========================
-# 3. Detect landmarks
-# =========================
-result = detector.detect(mp_image)
-
-# =========================
-# 4. Draw landmarks
-# =========================
-if result.face_landmarks:
-
-    h, w, _ = opencv_image.shape
-
-    print(f"Detected {len(result.face_landmarks)} face(s)")
-
-    for face_index, face_landmarks in enumerate(result.face_landmarks):
-
-        print(f"\nFace {face_index}:")
-
-        # -------------------------
-        # Print ONLY the first 10
-        # -------------------------
-        for landmark_index, landmark in enumerate(face_landmarks[:10]):
-
-            x = int(landmark.x * w)
-            y = int(landmark.y * h)
-
-            print(
-                f"Landmark {landmark_index}: "
-                f"normalized=({landmark.x:.4f}, "
-                f"{landmark.y:.4f}, "
-                f"{landmark.z:.4f}) "
-                f"pixels=({x}, {y})"
-            )
-
-        # -------------------------
-        # Draw ALL landmarks
-        # -------------------------
-        for landmark in face_landmarks:
-
-            x = int(landmark.x * w)
-            y = int(landmark.y * h)
-
-            cv2.circle(
-                opencv_image,
-                (x, y),
-                1,
-                (0, 255, 0),
-                -1
-            )
-
-
-else:
-    print("No face detected.")
-
-# =========================
-# 5. Show result
-# =========================
-cv2.imshow("Face Landmarks", opencv_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-'''
